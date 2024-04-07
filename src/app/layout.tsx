2b0f828f11e24f1d86933a910cb974ed0ca9a918
Vladimir Iglovikov <iglovikov@gmail.com>
2024-04-06 18:04:48 -0700

    Cleanup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,12 +17,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" type="image/svg+xml" href="/albumentations_logo.svg" />
-        {/* Fallback for browsers that do not support SVG favicons */}
-        <link rel="alternate icon" type="image/png" href="/albumentations_logo.png" />
-        <title>Stats for Albumentations</title>
-      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
